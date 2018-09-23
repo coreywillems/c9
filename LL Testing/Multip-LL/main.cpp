22097d08c7db7c58ec5fa4e1d4e6f8f433ea55b6
coreywillems <coreywillems@gmail.com>
2018-09-23 12:53:48 -0700

    Restarted and tested

--- LL Testing/Multip-LL/main.cpp
+++ LL Testing/Multip-LL/main.cpp
@@ -3,9 +3,11 @@
 #include "testClass.h"
 #include <iostream>
 
+//testing
 using namespace std;
 
-int main() {
+int main()
+{
   // intNode for intClass
   intNode *a = new intNode(3, nullptr);
   intNode *b = new intNode(20, nullptr);
