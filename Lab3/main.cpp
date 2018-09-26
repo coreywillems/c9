7f559d4133646cba3303385903c0799e642a2bb3
coreywillems <coreywillems@gmail.com>
2018-09-25 17:42:58 -0700

    updated the SSL class and functions

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -2,25 +2,52 @@
 #include "node.h"
 using namespace std;
 
-
 int main() {
-    Node <int>* p;
-    p = front;
-    
-    Node* corey = new Node(2, nullptr);
-    
-    Node* madeline = new Node(6, nullptr);
+
+    Node *corey = new Node(2, nullptr);
+
+    Node *madeline = new Node(6, nullptr);
     corey->next = madeline;
-    
-    Node* sake = new Node(90, nullptr); 
+
+    Node *sake = new Node(90, nullptr);
     madeline->next = sake;
-    
-    Node* mochi = new Node(33, nullptr);
+
+    Node *mochi = new Node(33, nullptr);
     sake->next = mochi;
-    
-    Node* plants = new Node(20, nullptr);
+
+    Node *plants = new Node(20, nullptr);
     mochi->next = plants;
     plants->next = nullptr;
-    
+
+    Node *lizzard = new Node(23, nullptr);
+    lizzard->next = corey;
+
+    Node *snake = new Node(99, nullptr);
+    madeline->next = snake;
+    snake->next = sake;
+
+
+
+
+    SLL first;
+    first.insertFront(10);
+    first.insertFront(5);
+    first.insertFront(22);
+
+    SLL* second = new SLL();
+    second->insertFront(12);
+    second->insertFront(99);
+    second->insertFront(2);
+    second->insertFront(23);
+
+    printNodeList(lizzard);
+    first.printList();
+
+    second->insertMiddle(lizzard);
+    second->printList();
+
+
+
+
     return 0;
 }
--- Lab3/node.h
+++ Lab3/node.h
@@ -1,29 +1,77 @@
 #ifndef NODE_H
 #define NODE_H
+#include <iostream>
+#include <printf.h>
 using namespace std;
 
 class Node {
     public:
         int data;
         Node *next;
-        
-        Node(int d=0, Node* n = nullptr) {
-            data = d;
-            next = n;
-        }
+
+        Node(int d = 0, Node *n = nullptr)
+    {
+        data = d;
+        next = n;
+    }
 };
 
 class SLL {
-  private: 
-  Node* front;
-  
-  public:
-  SLL() { front = nullptr; }
+    private:
+    Node* front;
+
+    public:
+    SLL() { front = nullptr; }
+
+    void insertFront(int item) {
+        Node* nn = new Node(item, nullptr);
+        if(front == NULL) {
+            front = nn;
+        }
+        else {
+            nn->next = front;
+            front = nn;
+        }
+    }
+
+    void insertMiddle(Node* front) {
+        Node* slowPTR = front;
+        Node* fastPTR = front;
+
+        if (front!=NULL) {
+            while(fastPTR != NULL && fastPTR->next != NULL) {
+                fastPTR = fastPTR->next->next;
+                slowPTR = slowPTR->next;
+            }
+            printf("middle is [%d]\n\n", slowPTR->data);
+        }
+    }
+/*     void insertToMiddle(int item) {
+        Node* nn = new Node(item, nullptr);
+        count;
+        while(nn != NULL && nn/2=0) {
+            nn = nn->next;
+        }
+    } */
+
+    void printList () {
+        Node *name = front;
+        cout << endl << "Sll List" << endl;
+
+        while(name != NULL) {
+            cout << name->data << endl;
+            name = name->next;
+        }
+    }
 };
 
-void printList() {
-    
-}
 
+void printNodeList(Node* name) {
+
+    while(name != NULL) {
+        cout << name->data << endl;
+        name = name->next;
+    }
+}
 
 #endif
