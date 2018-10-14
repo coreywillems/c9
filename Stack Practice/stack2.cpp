bdea0423c937aa13bcd6f89b35f3ca54161f9119
coreywillems <coreywillems@gmail.com>
2018-10-14 20:03:16 +0000

    stack practise

---
+++ Stack Practice/stack2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+string stream = "";
+stack <char> ops;
+stack <int> nums;
+
+void postfix(string exp) {
+    
+}
+
+void infix(string exp) {
+    
+}
+
+int main() {
+    
+}
